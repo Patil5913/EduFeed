637ab0d2d5006946baa640dcba9da4bd677931fb
Jigyanshu Don <jigyanshukumawat@gmail.com>
2024-02-22 12:59:35 +0530

    Add routing and new pages for student feedback

---
+++ src/pages/student/components/NonAcademicFeedback.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NonAcademicFeedback = () => {
+  return (
+    <div>
+      This is Non Academic Feedback
+    </div>
+  )
+}
+
+export default NonAcademicFeedback
