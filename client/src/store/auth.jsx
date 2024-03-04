1e2be603eeebad32f6e6ea753fd5300136e6ec3f
parth <contactparth2664@gmail.com>
2024-03-04 20:56:05 +0530

    auth

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,19 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {createContext} from 'react'
+
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children})=>{
-
+    const [user,setUser]=useState()
     const storeTokenInLs=(serverToken)=>{
         return localStorage.setItem("token",serverToken)
     }
+
+    const userAuthentication = async()=>{
+        try {
+            const response = await fetch("http://localhost:6969/login",{
+                method:"GET",
+                headers:{
+                    Authorization:`Bearer ${token}`,
+                },
+            })
+            if (response.ok){
+                const data = await response.json ();
+            }
+        } catch (error) {
+            console.error("error fetching user data")
+            setUser(data)
+        }
+    };
+    useEffect(()=>{
+        userAuthentication();
+    },[])
 
     return <AuthContext.Provider value={{storeTokenInLs}}>
     {children}
     </ AuthContext.Provider >
 }
 export const useAuth = ()=>{
-    
+
     return useContext(AuthContext)
 }
