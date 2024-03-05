d351e738e18dd82a5a660ba8c5add352b7deed0b
parth <contactparth2664@gmail.com>
2024-03-06 00:48:21 +0530

    Add academic model and routes for academic feedback

---
+++ server/models/academic.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const academicfeedbackSchema = new mongoose.Schema({
+  mentorEmail: {
+    type: String,
+  },
+  mentorQuestions: [
+    {
+      type: String,
+    },
+  ],
+  studentEmail: {
+    type: String,
+  },
+  answers: [
+    {
+      selectedOption: {
+        type: String,
+        enum: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"],
+      },
+    },
+  ],
+  comments: {
+    type: String,
+  },
+});
+
+const academicFeedbackModel = mongoose.model('Academic Feedback', academicfeedbackSchema);
+
+module.exports = academicFeedbackModel;
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const loginController = require('../controllers/login.controller.js');
 const registerController = require('../controllers/register.controller.js');
-const authenticate = require('../middleware/authenticate.js')
+const academicfeedbackController = require('../controllers/academic.controller.js');
+// const authenticate = require('../middleware/authenticate.js')
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -10,5 +11,10 @@ router.get("/", (req, res) => {
 router.post("/login", loginController.login);
 router.post("/register", registerController.register);
 router.get('/getUser', registerController.getAllUsers);
+router.post('/submitquestion', academicfeedbackController.submitQuestions);
+router.get('/getquestion', academicfeedbackController.getMentorQuestions);
+router.post('/submitfeedback', academicfeedbackController.submitFeedback);
+router.get('/getdetails', academicfeedbackController.getStudentSubmittedDetail);
+router.get('/getfeedback', academicfeedbackController.getFeedback);
 
 module.exports = router;
