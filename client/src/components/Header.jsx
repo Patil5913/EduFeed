937b8131917ebe9a5b39a31840095b7ff63b6440
Jigyanshu Don <jigyanshukumawat@gmail.com>
2024-03-07 14:43:14 +0530

    add ui for feedback

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.png";
 import Login from '../pages/login/Login'
 const Header = () => {
   return (
-    <header class=" bg-black  text-white shadow-lg hidden md:block top-2 z-10 sticky mb-10 hover:shadow-indigo-500/40 ">
+    <header class=" bg-black  text-white shadow-lg hidden md:block top-2 z-10 sticky mb-1 ">
       <div class="container mx-auto flex items-center h-24 ">
         <a href="" class="flex items-center justify-center ">
           <img class="h-24" src={Logo} alt="" />
@@ -22,7 +22,7 @@ const Header = () => {
           {/* <div
     class="inline-block  min-h-[1em] w-0.5 self-stretch bg-white opacity-100 dark:opacity-50"></div> */}
           
-          <Link to="/Login" class="relative border-2 border-dashed border-[#f98d17]  px-10 py-3 text-[#f98d17] text-base font-bold nded-full overflow-hidden bg-transparent rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#f98d17] before:to-[#fbaf5d] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+          <Link to="/Login" class="relative border-2 border-dashed border-[#f98d17]  px-10 py-3 text-[#f98d17] text-base font-bold nded-full overflow-hidden bg-transparent rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#f98d17] before:to-[#c6d66b] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
             Login
             </Link>
         </div>
